a11bfbab3c44089f586bb9453caab48f485fb5d2
zedan <zedan@moltaqa.net>
2025-07-21 10:40:08 +0300

    finish estimate

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { ReportsService } from './reports.service';
@@ -15,6 +17,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from 'src/guards/admin.gurad';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -32,5 +35,10 @@ export class ReportsController {
   @Patch(':id')
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(+id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { User } from 'src/users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -28,5 +29,32 @@ export class ReportsService {
     if (!report) throw new NotFoundException('Report Not Found');
     report.approved = approved;
     return await this.reportsRepository.save(report);
+  }
+
+  async createEstimate(estimateDto: GetEstimateDto) {
+    return this.reportsRepository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where(
+        `(make = :make) AND 
+        (model=:model) AND 
+        (lng - :lng BETWEEN -5 AND 5) AND 
+        (lat - :lat BETWEEN -5 AND 5) AND 
+        (year - :year BETWEEN -3 AND 3) AND
+        (approved IS TRUE)
+         
+        `,
+        {
+          make: estimateDto.make,
+          model: estimateDto.model,
+          lng: estimateDto.lng,
+          lat: estimateDto.lat,
+          year: estimateDto.year,
+        },
+      )
+      .orderBy('ABS(mileage - :mileage)', 'ASC')
+      .setParameters({ mileage: estimateDto.mileage })
+      .limit(3)
+      .getRawOne();
   }
 }
